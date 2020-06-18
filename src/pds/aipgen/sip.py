2c022b6cfd07f34cd4a002658df568c6636f0141
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-06-18 13:17:52 -0700

    Add timestamp to referenced AIP LIDVID

--- src/pds/aipgen/sip.py
+++ src/pds/aipgen/sip.py
@@ -213,7 +213,7 @@ def _writeLabel(
     etree.SubElement(deep, prefix + 'manifest_checksum').text = digest
     etree.SubElement(deep, prefix + 'checksum_type').text = 'MD5'
     etree.SubElement(deep, prefix + 'manifest_url').text = SIP_MANIFEST_URL + manifestFile
-    etree.SubElement(deep, prefix + 'aip_lidvid').text = AIP_PRODUCT_URI_PREFIX + logicalID.split(':')[-1] + '_v' + versionID + '::1.0'
+    etree.SubElement(deep, prefix + 'aip_lidvid').text = AIP_PRODUCT_URI_PREFIX + logicalID.split(':')[-1] + '_v' + versionID + '_' + timestamp.date().strftime('%Y%m%d') + '::1.0'
 
     aipMD5 = getMD5(aipFile) if aipFile else '00000000000000000000000000000000'
     etree.SubElement(deep, prefix + 'aip_label_checksum').text = aipMD5
