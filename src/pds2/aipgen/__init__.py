d9133c09735049fd3ec39e36c45f21094528e5eb
Sean Kelly <kelly@seankelly.biz>
2021-09-27 19:46:20 -0500

    Strip

--- src/pds2/aipgen/__init__.py
+++ src/pds2/aipgen/__init__.py
@@ -31,4 +31,4 @@
 import pkg_resources
 
 
-__version__ = VERSION = pkg_resources.resource_string(__name__, "VERSION.txt").decode("utf-8")
+__version__ = VERSION = pkg_resources.resource_string(__name__, "VERSION.txt").decode("utf-8").strip()
